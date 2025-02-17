835a68ee3e7d82b43a640b997ab0178a92a36734
Maite <anamaitegarciacorrea@gmail.com>
2025-02-17 23:00:01 +0100

    Arreglo cosas de tailwind

--- index.js
+++ index.js
@@ -115,6 +115,18 @@
 
         // document.body.append(info);
 
+  //MENÚ HAMBURGUESA
+  const menuBtn = document.getElementById('menu-btn');
+        const closeBtn = document.getElementById('close-btn');
+        const mobileMenu = document.getElementById('mobile-menu');
+
+        menuBtn.addEventListener('click', () => {
+            mobileMenu.classList.remove('-translate-x-full');
+        });
+
+        closeBtn.addEventListener('click', () => {
+            mobileMenu.classList.add('-translate-x-full');
+        });
 
 
 
